1a999cbe3cad1bdaba491686541188586b707aa0
Md Redwan Islam <redwanislamsiam13@gmail.com>
2026-01-21 17:41:40 +0600

    bug resolved-1

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import AppRoutes from './routes/AppRoutes.jsx'
-import AuthContext, { AuthProvider } from './context/authContext.jsx'
+import AuthContext, { AuthProvider } from './context/AuthContext.jsx'
 
 
 createRoot(document.getElementById("root")).render(
